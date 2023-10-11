97e82bb1ffcb1c83699bccf3924b4788a6c357f4
Aecy <devsnake@hotmail.com>
2023-10-11 20:12:20 +0200

    add: tailwindcss

--- html_partials/header.html.php
+++ html_partials/header.html.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title ?? 'PSF 1' ?></title>
-  </head>
-  <body>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
--- public/admin/login.php
+++ public/admin/login.php
@@ -24,24 +24,33 @@ if (is_post()) {
 
 <?php partial("header", ['title' => "Se connecter"]) ?>
 
-<h1>Se connecter</h1>
-<form method="post">
+<div class="min-w-screen min-h-screen flex justify-center items-start bg-gray-100">
+    <div class="bg-white shadow p-8 mt-12">
 
-    <?php if (isset($errors['credentials'])): ?>
-        <p><?= $errors['credentials'] ?></p>
-    <?php endif ?>
+        <h1 class="text-lg mb-4">Se connecter</h1>
 
-    <p>
-        <label for="name">Nom d'utilisateur</label>
-        <input id="name" type="text" name="name" required autofocus>
-    </p>
-    <p>
-        <label for="password">Mot de passe</label>
-        <input id="password" type="password" name="password" required>
-    </p>
-    <p>
-        <button type="submit">Se connecter</button>
-    </p>
-</form>
+        <form method="post">
+            <?php if (isset($errors['credentials'])): ?>
+                <p class="border border-red-900 w-full bg-red-100 text-red-900 text-xs mb-4 px-3 py-1">
+                    <?= $errors['credentials'] ?>
+                </p>
+            <?php endif ?>
+
+            <div class="mb-4">
+                <label for="name" class="block text-sm mb-px">Nom d'utilisateur</label>
+                <input id="name" type="text" name="name" class="border focus:border-black px-3 py-1 outline-none w-full" required autofocus>
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="block text-sm mb-px">Mot de passe</label>
+                <input id="password" type="password" name="password" class="border focus:border-black px-3 py-1 outline-none w-full" required>
+            </div>
+
+            <div class="mt-8">
+                <button type="submit" class="border py-1 px-2">Se connecter</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 <?php partial("footer") ?>
