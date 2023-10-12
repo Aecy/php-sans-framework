fca4ed8f07b298ed28de3909c1dbf5bcb36abb6f
Aecy <devsnake@hotmail.com>
2023-10-12 15:11:22 +0200

    add: performance pdo

--- bootstrap.php
+++ bootstrap.php
@@ -31,6 +31,12 @@ function is_post(): bool
 
 function pdo(): PDO
 {
+    static $pdo;
+
+    if ($pdo) {
+        return $pdo;
+    }
+
 	$pdo = new PDO("mysql:host=localhost;dbname=phpsansframework", 'root', 'root');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
